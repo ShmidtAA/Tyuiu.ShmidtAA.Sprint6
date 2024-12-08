0a7e8ca1954a3e2371c639adbf9197af99ba8dd2
ShmidtAA <Tyuiu.ShmidtAA@outlook.com>
2024-12-09 03:00:01 +0500

    Task 7

---
+++ Tyuiu.ShmidtAA.Sprint6.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+using Tyuiu.ShmidtAA.Sprint6.Task7.V9.Lib;
+namespace Tyuiu.ShmidtAA.Sprint6.Task7.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = "D:\\Загрузки\\InPutDataFileTask7V9.csv";
+            DataService ds = new DataService();
+            int[] waitArray = { -14, -16, 8, 7, 7, -13, -17, 18, 11, 17 };
+            int[,] matrix = ds.GetMatrix(path);
+            int[] resultArray = new int[matrix.GetLength(0)];
+            
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                resultArray[i] = matrix[i, 2];
+            }
+            CollectionAssert.AreEqual(waitArray, resultArray);
+
+
+        }
+    }
+}
---
+++ Tyuiu.ShmidtAA.Sprint6.Task7.V9/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.ShmidtAA.Sprint6.Task7.V9
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
